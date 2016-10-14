410faea5eca5101440f9148bf93b0ef8a8b17024
Szymon Rybka <srybka@clickmeeting.com>
2016-10-14 17:43:37 +0200

    Change text style does not invoke auto scroll

--- src/angular2-auto-scroll.directive.ts
+++ src/angular2-auto-scroll.directive.ts
@@ -1,20 +1,26 @@
-import {Directive, ElementRef, HostListener, AfterContentInit, Input, OnDestroy} from '@angular/core';
+import {Directive, ElementRef, HostListener, AfterContentInit, Input, OnDestroy} from "@angular/core";
 
 @Directive({
-    selector: '[angular2-auto-scroll]',
+    selector: '[angular2-auto-scroll]'
 })
 export class Angular2AutoScroll implements AfterContentInit, OnDestroy {
     @Input('angular2-auto-scroll') lockYOffset = 10;
 
+    @Input() set observeAttributes(observeAttributes: boolean) {
+        this._observeAttributes = observeAttributes || false;
+    }
+
     private nativeElement: HTMLElement;
     private isLocked = false;
     private mutationObserver: MutationObserver;
+    private _observeAttributes: boolean = false;
 
     constructor(element: ElementRef) {
         this.nativeElement = element.nativeElement;
     }
 
-    @HostListener('scroll') private scrollHandler() {
+    @HostListener('scroll')
+    private scrollHandler() {
         const scrollFromBottom = this.nativeElement.scrollHeight - this.nativeElement.scrollTop - this.nativeElement.clientHeight;
         this.isLocked = scrollFromBottom > this.lockYOffset;
     }
@@ -25,7 +31,11 @@ export class Angular2AutoScroll implements AfterContentInit, OnDestroy {
                 this.nativeElement.scrollTop = this.nativeElement.scrollHeight;
             }
         });
-        this.mutationObserver.observe(this.nativeElement, {childList: true, subtree: true});
+        this.mutationObserver.observe(this.nativeElement, {
+            childList: true,
+            subtree: true,
+            attributes: this._observeAttributes
+        });
     }
 
     ngOnDestroy() {

3752c28db4be4604110289b6c5a0bcaecf8da456
Adrian Ber <beradrian@yahoo.com>
2018-01-08 19:51:46 +0200

    Use ng-packagr and create angular module

---
+++ public_api.ts
@@ -0,0 +1,1 @@
+export { NgxAutoScroll } from './src/ngx-auto-scroll.directive';
---
+++ src/ngx-auto-scroll.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { NgxAutoScroll } from './ngx-auto-scroll.directive';
+
+@NgModule({
+  exports: [NgxAutoScroll]
+})
+export class NgxAutoScrollModule {}
